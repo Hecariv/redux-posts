84d5ab1cdb71d45c77a03ff3c5053e2b1619b190
hecariv <hector.casr1992@gmail.com>
2021-04-01 16:07:52 +0200

    test postsReducer

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import { render, screen } from '@testing-library/react';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux'
+import App from "./App";
+import React from "react";
+
+
+
+const middlewares = [thunk];
+const mockstore = configureMockStore(middlewares);
+
+test('renders without crashing', () => {
+
+  const store = mockstore({ postsRed: { posts: [] }, commentsRed: {comments: []}});
+
+
+  render(
+      <Provider store={store}>
+        <App/>
+      </Provider>
+  );
+  const linkElement = screen.getByText("jkljlöjklkl")
+  expect(linkElement).toBeInTheDocument();
 });
